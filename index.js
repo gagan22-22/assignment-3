83887611e676bca3d1fff36168b045a0384253dc
gagan22-22 <157568072+gagan22-22@users.noreply.github.com>
2024-02-02 11:22:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// const title=document.getElementById('head');
+// console.log(title)
+
+function createuser(){
+    var lis=document.getElementById('i1').Value;
+    console.log(lis).Value;
+    var lis1=document.getElementById('i2').Value;
+    console.log(lis1).Value;
+}
+
+createuser();
